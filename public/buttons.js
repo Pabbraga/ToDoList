72ae453914ed5903f8c62dff9dc228f3572a4b72
Pabraga <103124116+Pabbraga@users.noreply.github.com>
2023-02-26 23:29:23 -0300

    first responsiveness changes

--- public/buttons.js
+++ public/buttons.js
@@ -1,27 +1,34 @@
 export default function createButtons() {
-    
-    function removeButton(commands) {
+
+    function getButtons() {
+        let buttons_container = document.createElement("div")
+        buttons_container.classList.add("icons")
+        buttons_container.appendChild(editButton())
+        buttons_container.appendChild(removeButton())
+        return buttons_container
+    }
+
+    function removeButton() {
         let remove_button = document.createElement("i")
         let buttonClasses = ["fa-solid", "fa-trash", "icon"]
         for(let i in buttonClasses) {
             remove_button.classList.add(buttonClasses[i])
         }
-        remove_button.onclick = commands
         return remove_button
     }
     
-    function editButton(commands) {
+    function editButton() {
         let edit_button = document.createElement("i")
         let buttonClasses = ["fa-solid", "fa-pen", "icon"]
         for(let i in buttonClasses) {
             edit_button.classList.add(buttonClasses[i])
         }
-        edit_button.onclick = commands
         return edit_button
     }
-    
+
     return {
         editButton,
-        removeButton
+        removeButton,
+        getButtons
     }
 }
--- public/note.js
+++ public/note.js
@@ -1,44 +1,30 @@
 export default function createNote() {
 
-    const stickNote = function(data, buttons, onClick) {
+    const stickNote = function(data, buttons_container) {
         let stickNote = data
         stickNote.classList.add("note")
         stickNote.id = Math.floor(Date.now() * Math.random()).toString(36)
-        stickNote.onclick = onClick
-        stickNote.insertAdjacentElement("beforeend", buttons)
+        stickNote.insertAdjacentElement("beforeend", buttons_container)
         return stickNote
     }
 
-    function addNote(buttons, onClick) {
+    function addNote(buttons_container) {
         let list = document.getElementById("myList")
         let title = document.getElementById("myTitle")
         let item = document.createElement("li")
         if(title) {
             item.innerHTML = title.value
-            list.appendChild(stickNote(item, buttons, onClick))
+            list.appendChild(stickNote(item, buttons_container))
         }
     }
     
     function editNote() {
-        // let item = document.getElementById(note)
-        // if(item) {
-    
-        // } else {
-        //    throw error
-        // }
-        console.log("editou!")
+        console.log('editou') 
     }
     
-    function removeNote(id) {
-        // let item = document.getElementById(id)
-        // item.remove()
-        console.log("removeu!")
-    }
-
     return {
         stickNote,
         addNote,
-        editNote,
-        removeNote
+        editNote
     }
 }
